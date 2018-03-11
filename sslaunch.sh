8f3c6033d51e13b696db9ff6045735458719bf22
zthxxx <zth_9451@qq.com>
2018-03-12 00:25:38 +0800

    fix: fix run on startup

--- sslaunch.sh
+++ sslaunch.sh
@@ -3,6 +3,7 @@
 # install & launch ssserver on ubuntu
 
 CONFIG_FILE="/etc/shadowsocks/config.json"
+SERVICE_FILE="/etc/systemd/system/shadowsocks.service"
 QRCOED_IPv4_FILE="/etc/shadowsocks/ss_qrcode_ipv4.log"
 QRCOED_IPv6_FILE="/etc/shadowsocks/ss_qrcode_ipv6.log"
 CIPHERS=(
@@ -28,6 +29,26 @@ install_shadowsocks() {
 
 install_qrencode() { apt-get install -y qrencode; }
 
+run_on_startup() {
+  if [ -f /etc/rc.local ]; then 
+    sed -i '/ssserver/d' /etc/rc.local
+    echo "ssserver -c $CONFIG_FILE -d start" >> /etc/rc.local
+  else
+    cat > "$SERVICE_FILE" <<-EOF
+			[Unit]
+			Description=Shadowsocks
+			
+			[Service]
+			TimeoutStartSec=0
+			ExecStart=/usr/bin/ssserver -c ${CONFIG_FILE}
+			
+			[Install]
+			WantedBy=multi-user.target
+		EOF
+    systemctl enable shadowsocks
+  fi
+}
+
 pre_install() {
   if ! command -v "ssserver" > /dev/null; then
     install_shadowsocks
@@ -35,15 +56,16 @@ pre_install() {
   if ! command -v "qrencode" > /dev/null; then
     install_qrencode
   fi
-  sed -i '/ssserver/d' /etc/rc.local
-  echo "ssserver -c $CONFIG_FILE -d start" >> /etc/rc.local
+  mkdir -p /etc/shadowsocks
+  touch "$CONFIG_FILE"
+  run_on_startup
 }
 
 launch() {
   cipher=`rand_cipher`
   passwd=`pwgen`
   port=`rand_port`
-  while port_check "$port"; do
+  while port_hold_check "$port"; do
     port=`rand_port`
   done
 
@@ -73,7 +95,7 @@ launch() {
 
   echo
   echo "shadowsocks server ipv4 config QRCode saved to $QRCOED_IPv4_FILE"
-  qrencode -t UTF8 -l H "$ss_ipv4_url" | tee "$QRCOED_IPv4_FILE"
+  qrencode -t UTF8 -l Q "$ss_ipv4_url" | tee "$QRCOED_IPv4_FILE"
   echo
   echo
   echo "shadowsocks server ipv6 config QRCode saved to $QRCOED_IPv6_FILE"
